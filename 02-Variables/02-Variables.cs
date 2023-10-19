453a9921b4a19897597a38daa303cc0dcaab9ada
THE-GGOAT <147569918+THE-GGOAT@users.noreply.github.com>
2023-10-20 10:34:48 +1100

    went well

--- 02-Variables/02-Variables.cs
+++ 02-Variables/02-Variables.cs
@@ -14,6 +14,7 @@ Main Tasks:
 
 Questions:
 1. What is a variable?
+somthing thats value can be change or be anything
 2. What is a string?
 
 Extension Tasks:
@@ -47,7 +48,25 @@ namespace ProgrammingExercisesIST
 ===============================================================================
 FEEDBACK AREA: Paste your AI feedback here before submission:
 ===============================================================================
+Feedback on your C# Programming Task
 
+Code:
+
+Your code is well-structured and follows the basic structure of a C# console application.
+You have correctly declared a string variable and assigned it a value.
+You have also correctly used the variable in a console output operation.
+However, you did not complete the extension tasks. Try creating two string variables and using them in the same message. Also, use Google to work out how to change a variable to uppercase or lowercase before outputting it.
+Code Commenting:
+
+Your code is missing comments. It's a good practice to include comments that explain what your code is doing. This will be especially helpful as your code gets more complex.
+Answers to Questions:
+
+Your answer to what a variable is correct but could be more detailed. For example, you could mention that a variable is a storage location in memory that holds a value which can be changed during program execution.
+You did not answer the question about what a string is. A string in C# is a sequence of characters enclosed in double quotes.
+Extension Tasks:
+
+You did not complete the extension tasks. Remember to always attempt these tasks as they are designed to help you further understand the topic.
+Keep practicing and exploring more concepts in C#. Your progress is commendable.
 
 
 ===============================================================================
--- 03-Variables2/03-DataTypes.cs
+++ 03-Variables2/03-DataTypes.cs
@@ -44,7 +44,11 @@ namespace ProgrammingExercisesIST
         static void Main(string[] args)
         {
             // Your code goes below here
-
+            int name = 983657;
+            double name2 = 1.5;
+            bool name3 = false;
+            Console.WriteLine(name + name2);
+            Console.ReadLine();
         }
     }
 }
@@ -53,7 +57,22 @@ namespace ProgrammingExercisesIST
 ===============================================================================
 FEEDBACK AREA: Paste your AI feedback here before submission:
 ===============================================================================
+Feedback on your Project 03: Data Types
 
+Code
+
+Good job on creating the variables with the correct data types. However, it is recommended to use more descriptive variable names. For example, instead of name, name2, and name3, you could use wholeNumber, decimalNumber, and booleanValue. This would make your code more readable.
+You've correctly used the Console.WriteLine function to output the values of your variables. However, the task asked you to output a single message that includes all three of your variables. You've only included two (name and name2) and the output is not in a sentence format.
+Code Commenting
+
+There are no comments in your code. Adding comments to your code can help others understand what each part of your code does. It's a good practice to start doing this early.
+Answers to Questions
+
+You didn't provide answers to the questions in your submission. Remember to answer them next time. They're designed to help you understand the concepts better.
+Extension Tasks
+
+You didn't complete the extension tasks. I encourage you to attempt these in the future as they are designed to challenge you and enhance your learning.
+Please consider these points for your next assignment. Keep practicing and you will get better!
 
 
 ===============================================================================
