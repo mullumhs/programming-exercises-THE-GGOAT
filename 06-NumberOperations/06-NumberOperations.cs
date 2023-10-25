417d982a9ebd35d5ff7aa6f0767ac9a8003b5f9d
THE-GGOAT <147569918+THE-GGOAT@users.noreply.github.com>
2023-10-25 12:32:06 +1100

    Update 06-NumberOperations.cs

--- 06-NumberOperations/06-NumberOperations.cs
+++ 06-NumberOperations/06-NumberOperations.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+
 
 /*
 ===============================================================================
@@ -53,7 +55,19 @@ namespace ProgrammingExercisesIST
             double x = Convert.ToDouble(Console.ReadLine());
 
             // Your code goes below here
+            Console.WriteLine("Please enter a number for Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
 
+            //Add
+            Console.WriteLine($"{x} plus {y} = {x + y}");
+
+            Console.WriteLine($"{x} minus {y} = {x - y}");
+
+            Console.WriteLine($"{x} times {y} = {x * y}");
+
+            Console.WriteLine($"{x} divided {y} = {x / y}");
+
+            Console.ReadLine();
         }
     }
 }
